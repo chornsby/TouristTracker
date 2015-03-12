c6da330aa641a921c6d9e44af68d5b75cbc58475
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2015-03-12 20:26:21 +0200

    Implement ContentProvider based on notes from Android tutorial

--- app/src/main/java/com/chornsby/touristtracker/data/TrackerContract.java
+++ app/src/main/java/com/chornsby/touristtracker/data/TrackerContract.java
@@ -1,28 +1,22 @@
 package com.chornsby.touristtracker.data;
 
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class TrackerContract {
-//    public static final String CONTENT_AUTHORITY = "com.chornsby.touristtracker";
-//    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String CONTENT_AUTHORITY = "com.chornsby.touristtracker";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-//    public static final String PATH_LOCATION = "location";
-
-    // Normalise all dates in the database to make querying easier
-//    public static long normalizeDate(long startDate) {
-//        // normalize the start date to the beginning of the (UTC) day
-//        Time time = new Time();
-//        time.set(startDate);
-//        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
-//        return time.setJulianDay(julianDay);
-//    }
+    public static final String PATH_LOCATION = "location";
 
     public static final class LocationEntry implements BaseColumns {
-//        public static final Uri CONTENT_URI =
-//                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
-//        public static final String CONTENT_TYPE =
-//                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
         public static final String TABLE_NAME = "location";
 
@@ -34,6 +28,10 @@ public class TrackerContract {
         public static final String COLUMN_PROVIDER = "provider";
         public static final String COLUMN_SPEED = "speed";
         public static final String COLUMN_TIME = "time";
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
 
---
+++ app/src/main/java/com/chornsby/touristtracker/data/TrackerProvider.java
@@ -0,0 +1,140 @@
+package com.chornsby.touristtracker.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+public class TrackerProvider extends ContentProvider {
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private TrackerDbHelper mOpenHelper;
+
+    static final int LOCATION = 100;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        matcher.addURI(TrackerContract.CONTENT_AUTHORITY, TrackerContract.PATH_LOCATION, LOCATION);
+
+        return matcher;
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new TrackerDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Cursor cursor;
+
+        switch (sUriMatcher.match(uri)) {
+            case LOCATION:
+                cursor = mOpenHelper.getReadableDatabase().query(
+                        TrackerContract.LocationEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case LOCATION:
+                return TrackerContract.LocationEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case LOCATION:
+                long id = db.insert(TrackerContract.LocationEntry.TABLE_NAME, null, values);
+                if (id > 0) {
+                    returnUri = TrackerContract.LocationEntry.buildWeatherUri(id);
+                } else {
+                    throw new SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+
+        if (selection == null) selection = "1";
+
+        switch (match) {
+            case LOCATION:
+                rowsDeleted = db.delete(
+                        TrackerContract.LocationEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match) {
+            case LOCATION:
+                rowsUpdated = db.update(
+                        TrackerContract.LocationEntry.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+}
