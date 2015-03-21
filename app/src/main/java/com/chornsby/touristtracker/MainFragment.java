bf0a818c6c88d4fdca7ca07a6ca8a3998913ff9a
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2015-03-21 15:55:53 +0200

    Remove Google Play Services callbacks from MainFragment

--- app/src/main/java/com/chornsby/touristtracker/MainFragment.java
+++ app/src/main/java/com/chornsby/touristtracker/MainFragment.java
@@ -1,31 +1,22 @@
 package com.chornsby.touristtracker;
 
 import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.esri.android.map.MapOptions;
 import com.esri.android.map.MapView;
 import com.esri.android.map.event.OnStatusChangedListener;
 import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationServices;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainFragment extends Fragment
-        implements ConnectionCallbacks, OnConnectionFailedListener{
+public class MainFragment extends Fragment {
 
     private static final String LOG_TAG = MainFragment.class.getSimpleName();
 
@@ -35,32 +26,26 @@ public class MainFragment extends Fragment
     public MainFragment() {
     }
 
-    GoogleApiClient mGoogleApiClient;
-    Location mLastLocation;
-    MapView mMapView;
-    FloatingActionButton mAddPhoto;
-    FloatingActionButton mAddNote;
-    Intent serviceIntent;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        mMapView = (MapView) rootView.findViewById(R.id.map);
-        mMapView.setOnStatusChangedListener(new OnStatusChangedListener() {
+        final MapView mapView = (MapView) rootView.findViewById(R.id.map);
+        mapView.setOnStatusChangedListener(new OnStatusChangedListener() {
             private static final long serialVersionUID = 1L;
 
             @Override
             public void onStatusChanged(Object source, STATUS status) {
-                if (OnStatusChangedListener.STATUS.INITIALIZED == status && source == mMapView) {
-                    mGoogleApiClient.connect();
+                if (OnStatusChangedListener.STATUS.INITIALIZED == status && source == mapView) {
+                    // TODO: Get latest location from database
+                    return;
                 }
             }
         });
 
-        mAddPhoto = (FloatingActionButton) rootView.findViewById(R.id.add_photo_fab);
-        mAddPhoto.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addPhoto = (FloatingActionButton) rootView.findViewById(R.id.add_photo_fab);
+        addPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -68,8 +53,8 @@ public class MainFragment extends Fragment
             }
         });
 
-        mAddNote = (FloatingActionButton) rootView.findViewById(R.id.add_note_fab);
-        mAddNote.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addNote = (FloatingActionButton) rootView.findViewById(R.id.add_note_fab);
+        addNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(), "Currently not implemented.", Toast.LENGTH_SHORT)
@@ -77,11 +62,9 @@ public class MainFragment extends Fragment
             }
         });
 
-        buildGoogleApiClient();
-
-        serviceIntent = new Intent(getActivity(), TrackerService.class);
-        serviceIntent.setAction(TrackerService.ACTION_RESUME);
-        getActivity().startService(serviceIntent);
+        Intent intent = new Intent(getActivity(), TrackerService.class);
+        intent.setAction(TrackerService.ACTION_RESUME);
+        getActivity().startService(intent);
 
         return rootView;
     }
@@ -98,51 +81,5 @@ public class MainFragment extends Fragment
                     getActivity(), "Received some text!", Toast.LENGTH_SHORT
             ).show();
         }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-        if (mLastLocation != null) {
-            MapOptions mapOptions = new MapOptions(
-                    MapOptions.MapType.STREETS,
-                    mLastLocation.getLatitude(),
-                    mLastLocation.getLongitude(),
-                    18
-            );
-            mMapView.setMapOptions(mapOptions);
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d(LOG_TAG, "GooglePlayServices connection suspended");
-        Log.d(LOG_TAG, "Trying to reconnect");
-
-        // Try to reconnect
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.e(LOG_TAG, "GooglePlayServices connection failed: " + connectionResult.getErrorCode());
     }
 }
