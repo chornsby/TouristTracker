1ce9a9e903d895aa24f708c9c2852456554409f9
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2015-04-07 15:13:19 +0300

    Re-implement mapsforge MapView
    Add mapsforge MapView to fragment_main.xml
    Add new permissions to read/write to external storage
    Center mapview and set zoom level on fragment start

--- app/src/main/java/com/chornsby/touristtracker/MainFragment.java
+++ app/src/main/java/com/chornsby/touristtracker/MainFragment.java
@@ -1,7 +1,10 @@
 package com.chornsby.touristtracker;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,7 +12,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.chornsby.touristtracker.data.TrackerContract;
 import com.getbase.floatingactionbutton.FloatingActionButton;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapDataStore;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+
+import java.io.File;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -21,13 +37,36 @@ public class MainFragment extends Fragment {
     private static final int ADD_PHOTO_REQUEST_CODE = 1;
     private static final int ADD_NOTE_REQUEST_CODE = 2;
 
+    private MapView mMapView;
+    private TileCache mTileCache;
+    private TileRendererLayer mTileRendererLayer;
+
     public MainFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Initialise the GraphicsFactory for Mapsforge
+        AndroidGraphicFactory.createInstance(getActivity().getApplication());
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.map_view);
+        mMapView.setClickable(true);
+        mMapView.setBuiltInZoomControls(false);
+        mMapView.getMapScaleBar().setVisible(true);
+
+        mTileCache = AndroidUtil.createTileCache(
+                getActivity(),
+                "mapcache",
+                mMapView.getModel().displayModel.getTileSize(),
+                1f,
+                mMapView.getModel().frameBufferModel.getOverdrawFactor()
+        );
+
+        Toast.makeText(getActivity(), mMapView.getDimension().toString(), Toast.LENGTH_SHORT).show();
 
         FloatingActionButton addPhoto = (FloatingActionButton) rootView.findViewById(R.id.add_photo_fab);
         addPhoto.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +94,46 @@ public class MainFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        centerMapAtLatestLocation();
+        mMapView.getModel().mapViewPosition.setZoomLevel((byte) 16);
+
+        // TODO: Pre-install this file
+        File mapFile = new File(Environment.getExternalStorageDirectory(), "finland.map");
+        MapDataStore mapDataStore = new MapFile(mapFile);
+
+        mTileRendererLayer = new TileRendererLayer(
+                mTileCache,
+                mapDataStore,
+                mMapView.getModel().mapViewPosition,
+                false,
+                true,
+                AndroidGraphicFactory.INSTANCE
+        );
+        mTileRendererLayer.setXmlRenderTheme(InternalRenderTheme.OSMARENDER);
+
+        mMapView.getLayerManager().getLayers().add(mTileRendererLayer);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.getLayerManager().getLayers().remove(mTileRendererLayer);
+        mTileRendererLayer.onDestroy();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mTileCache.destroy();
+        mMapView.getModel().mapViewPosition.destroy();
+        mMapView.destroy();
+        AndroidGraphicFactory.clearResourceMemoryCache();
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == ADD_PHOTO_REQUEST_CODE) {
@@ -66,5 +145,39 @@ public class MainFragment extends Fragment {
                     getActivity(), "Received some text!", Toast.LENGTH_SHORT
             ).show();
         }
+    }
+
+    private void centerMapAtLatestLocation() {
+        // Retrieve relevant records from the database
+        Uri uri = TrackerContract.LocationEntry.CONTENT_URI;
+        String[] projection = {
+                TrackerContract.LocationEntry.COLUMN_LATITUDE,
+                TrackerContract.LocationEntry.COLUMN_LONGITUDE,
+                TrackerContract.LocationEntry.COLUMN_ACCURACY,
+        };
+
+        // Select only Location entries that are accurate to within 30m
+        String selection = TrackerContract.LocationEntry.COLUMN_ACCURACY + " < ?";
+        String[] selectionArgs = {"30"};
+
+        Cursor c = getActivity().getContentResolver().query(
+                uri,
+                projection,
+                selection,
+                selectionArgs,
+                null
+        );
+
+        final int LATITUDE_INDEX = c.getColumnIndex(TrackerContract.LocationEntry.COLUMN_LATITUDE);
+        final int LONGITUDE_INDEX = c.getColumnIndex(TrackerContract.LocationEntry.COLUMN_LONGITUDE);
+
+        if (c.moveToLast()) {
+            double latitude = c.getDouble(LATITUDE_INDEX);
+            double longitude = c.getDouble(LONGITUDE_INDEX);
+
+            mMapView.getModel().mapViewPosition.setCenter(new LatLong(latitude, longitude));
+        }
+
+        c.close();
     }
 }
