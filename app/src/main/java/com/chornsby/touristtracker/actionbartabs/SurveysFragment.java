45d53238f557f871506afca025e4b0f962b7646b
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2015-05-06 21:48:23 +0300

    Improve tabbed navigation and add survey listview

--- app/src/main/java/com/chornsby/touristtracker/actionbartabs/SurveysFragment.java
+++ app/src/main/java/com/chornsby/touristtracker/actionbartabs/SurveysFragment.java
@@ -1,14 +1,21 @@
 package com.chornsby.touristtracker.actionbartabs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.chornsby.touristtracker.R;
+import com.chornsby.touristtracker.survey.FirstDaySurveyActivity;
 
 public class SurveysFragment extends Fragment {
+
+    private ListView mListView;
 
     public SurveysFragment() {}
 
@@ -16,6 +23,23 @@ public class SurveysFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_surveys, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_surveys, container, false);
+
+        String[] surveys = getResources().getStringArray(R.array.survey_names_array);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                getActivity(), android.R.layout.simple_list_item_1, surveys
+        );
+
+        mListView = (ListView) rootView.findViewById(R.id.survey_list_view);
+        mListView.setAdapter(arrayAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getActivity(), FirstDaySurveyActivity.class));
+            }
+        });
+
+        return rootView;
     }
 }
