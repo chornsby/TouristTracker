165b4ba33e035e3963b83fa3dce7e2dd935f5e7f
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2015-10-17 12:07:00 +0300

    Convert survey view to basic set of CardView elements
    Add links to e-lomake forms from the app
    Remove old list view
    Hardcode values for testing

--- app/src/main/java/com/chornsby/touristtracker/actionbar/fragments/SurveysFragment.java
+++ app/src/main/java/com/chornsby/touristtracker/actionbar/fragments/SurveysFragment.java
@@ -1,21 +1,20 @@
 package com.chornsby.touristtracker.actionbar.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 
 import com.chornsby.touristtracker.R;
-import com.chornsby.touristtracker.survey.FirstDaySurveyActivity;
 
 public class SurveysFragment extends Fragment {
 
-    private ListView mListView;
+    private Button backgroundSurvey;
+    private Button walkabilitySurvey;
 
     public SurveysFragment() {}
 
@@ -25,21 +24,30 @@ public class SurveysFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_surveys, container, false);
 
-        String[] surveys = getResources().getStringArray(R.array.survey_names_array);
+        backgroundSurvey = (Button) rootView.findViewById(R.id.backgroundSurveyButton);
+        walkabilitySurvey = (Button) rootView.findViewById(R.id.walkabilitySurveyButton);
 
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                getActivity(), android.R.layout.simple_list_item_1, surveys
-        );
-
-        mListView = (ListView) rootView.findViewById(R.id.survey_list_view);
-        mListView.setAdapter(arrayAdapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        backgroundSurvey.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                startActivity(new Intent(getActivity(), FirstDaySurveyActivity.class));
+            public void onClick(View v) {
+                openWebPage("https://elomake.helsinki.fi/lomakkeet/63793/lomake.html");
+            }
+        });
+        walkabilitySurvey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("https://elomake.helsinki.fi/lomakkeet/63802/lomake.html");
             }
         });
 
         return rootView;
+    }
+
+    private void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
