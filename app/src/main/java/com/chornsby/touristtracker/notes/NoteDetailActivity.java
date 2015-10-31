643f36c53c5812ba7f733cda1cda02973e7047d8
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2015-10-31 12:13:44 +0200

    Customize NoteDetailActivity to add photo data to layout

--- app/src/main/java/com/chornsby/touristtracker/notes/NoteDetailActivity.java
+++ app/src/main/java/com/chornsby/touristtracker/notes/NoteDetailActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,6 +32,11 @@ public class NoteDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         Intent intent = getIntent();
 
