ec4a6a3f0dd5bba785de998036968752e8ec200b
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2015-11-09 21:39:21 +0200

    Allow capturing a photo as well as selecting one from the gallery

--- app/src/main/java/com/chornsby/touristtracker/notes/NoteDetailActivity.java
+++ app/src/main/java/com/chornsby/touristtracker/notes/NoteDetailActivity.java
@@ -1,27 +1,51 @@
 package com.chornsby.touristtracker.notes;
 
+import android.Manifest;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.chornsby.touristtracker.R;
+import com.chornsby.touristtracker.Utility;
 import com.chornsby.touristtracker.data.TrackerContract;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class NoteDetailActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CAMERA = 1;
+    private static final int SELECT_PICTURE = 2;
+
     TextView mRelativeDateTime;
     EditText mEditText;
+    FloatingActionButton mAddPhoto;
+    ImageView mImageView;
+
+    Uri mCurrentPhotoUri;
+    Uri mSelectedPhotoUri;
 
     String mSelection;
     String[] mSelectionArgs;
@@ -79,6 +103,151 @@ public class NoteDetailActivity extends AppCompatActivity {
 
         mRelativeDateTime = (TextView) findViewById(R.id.relative_date_time);
         mRelativeDateTime.setText(relativeDateTime);
+
+        mAddPhoto = (FloatingActionButton) findViewById(R.id.add_image_fab);
+        mAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        mImageView = (ImageView) findViewById(R.id.edit_photo);
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = {"Take Photo", "Choose from Library", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (items[which].equals("Take Photo")) {
+                    takePhoto();
+                } else if (items[which].equals("Choose from Library")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    );
+                    intent.setType("image/*");
+
+                    if (intent.resolveActivity(getPackageManager()) == null) {
+                        Snackbar.make(
+                                findViewById(R.id.activity_note_detail),
+                                "Could not find a gallery app",
+                                Snackbar.LENGTH_SHORT
+                        ).show();
+                        return;
+                    }
+                    startActivityForResult(intent, SELECT_PICTURE);
+                } else {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Check if there is an app capable of resolving the intent
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            Snackbar.make(
+                    findViewById(R.id.activity_note_detail),
+                    "Could not find a camera app",
+                    Snackbar.LENGTH_SHORT
+            ).show();
+            return;
+        }
+
+        // Verify permissions on Android 23
+        if (Utility.isStoragePermissionRequired(NoteDetailActivity.this)) {
+            Utility.requestStoragePermissions(NoteDetailActivity.this);
+            return;
+        }
+
+        // Create a file for the image
+        File imageFile;
+
+        try {
+            imageFile = Utility.createImageFile(NoteDetailActivity.this);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Snackbar.make(
+                    findViewById(R.id.activity_note_detail),
+                    "Problem saving image",
+                    Snackbar.LENGTH_SHORT
+            ).show();
+            return;
+        }
+
+        mCurrentPhotoUri = Uri.fromFile(imageFile);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mCurrentPhotoUri);
+
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                Utility.addToGallery(this, mCurrentPhotoUri);
+                mSelectedPhotoUri = mCurrentPhotoUri;
+                break;
+            case SELECT_PICTURE:
+                mSelectedPhotoUri = data.getData();
+                break;
+            default:
+                return;
+        }
+
+        InputStream imageStream;
+
+        try {
+            imageStream = getContentResolver().openInputStream(mSelectedPhotoUri);
+        } catch (FileNotFoundException e) {
+            Snackbar.make(
+                    findViewById(R.id.activity_note_detail),
+                    "Could not access the image",
+                    Snackbar.LENGTH_SHORT
+            );
+            return;
+        }
+
+        Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+        mImageView.setImageBitmap(selectedImage);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        boolean isReadGranted = false;
+        boolean isWriteGranted = false;
+
+        for (int i = 0; i < permissions.length; i++) {
+            String permission = permissions[i];
+            boolean isGranted = grantResults[i] == PackageManager.PERMISSION_GRANTED;
+
+            if (permission.equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                isReadGranted = isGranted;
+            } else if (permission.equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                isWriteGranted = isGranted;
+            }
+        }
+
+        if (isReadGranted && isWriteGranted) {
+            takePhoto();
+        }
     }
 
     private String createNote() {
@@ -94,6 +263,10 @@ public class NoteDetailActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mNote.note = mEditText.getText().toString();
+
+//        if (mSelectedPhotoUri != null) {
+//            mNote.imageUri = mSelectedPhotoUri.toString();
+//        }
 
         getContentResolver().update(
                 TrackerContract.NoteEntry.CONTENT_URI,
